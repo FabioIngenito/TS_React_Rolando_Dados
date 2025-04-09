6a715d1dff487d4e13fff67823428ad13ead32a6
Fabio Ingenito <fabio.ingenito@hotmail.com>
2025-04-09 18:30:09 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-
+// /* eslint-disable react-refresh/only-export-components */
+// Fast refresh only works when a file only exports components. Move your React context(s) to a separate file.eslint(react-refresh/only-export-components)
 // A atualização rápida só funciona quando um arquivo exporta apenas componentes. Mova seu(s) contexto(s) React para um arquivo separado.eslint(react-refresh/only-export-components)
 
 import React from 'react'
 import SorteioDados from "./components/SorteioDados";
 import {NumeroContext}  from './components/NumeroContext';
+
+// Esta linha foi para o arquivo "NumeroContext.ts" para resolver o problema de "Fast refresh":
 // export const NumeroContext = React.createContext(0);
 
 function App() {
